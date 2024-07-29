be60bbbc869d442f986680dcac926f4068b8525b
Kelvys Moura <kelvysmoura2@gmail.com>
2024-07-29 18:57:11 -0300

    Criando FK de user_types em users

--- api/models/UserModel.js
+++ api/models/UserModel.js
@@ -1,10 +1,19 @@
 const { DataTypes, Model } = require('sequelize');
 const connection = require('../config/connection');
+const UserTypesModel = require("./UserTypesModel");
 
 class UserModel extends Model {}
 
 UserModel.init(
   {
+    type_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: UserTypesModel,
+        key: 'id'
+      }
+    },
     is_active: {
         type: DataTypes.TINYINT(1),
         defaultValue: 0,
